34e4a20722d6e43c183f6f53d40e2cb74617879c
ankiambom <ankiambom1993@gmail.com>
2021-03-01 01:45:48 +0300

    header,footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+
+       <div class="card">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Panel footer</h5>
+               
+              </div>
+            </div>
